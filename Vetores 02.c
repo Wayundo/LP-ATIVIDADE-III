af071495ebc7c3a5f8b5b24ac1cd144427f644e0
Wayundo <107221656+Wayundo@users.noreply.github.com>
2023-11-25 13:08:24 -0300

    Update Vetores 02.c

--- Vetores 02.c
+++ Vetores 02.c
@@ -2,68 +2,66 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
-int main()
-{
 
+int main() {
     setlocale(LC_ALL, "portuguese");
 
-    int i, j;
-    char nome[5][300];
-    float notas[5][3];
-    int idade[5];
-    float media[5];
-    float somaNotas = 0;
+    int numeros[5];
+    int i;
+    int impar = 0;
+    int numerosNegativos = 0;
+    int maior = 0;
+    int menor = 9999;
+    int somaGeral = 0;
+    float mediaGeral;
+    int somaPar = 0;
+    int par = 0;
+    float mediaPar = 0;
 
-    for (i = 0; i < 5; i++)
-    {
-        printf("Nome do aluno: ");
-        gets(nome[i]);
+    for (i = 0; i < 5; i++) {
 
-        printf("Digite sua idade: ");
-        scanf("%d", &idade[i]);
+        printf("Digite o %dº número: ", i + 1);
+        scanf("%d", &numeros[i]);
 
-        for (j = 0; j < 3; j++)
-        {
-            printf("Digite uma nota: ");
-            scanf("%f", &notas[i][j]);
+if (numeros[i] % 2 == 0){
+    par++;
+    somaPar+=numeros[i];
+   }else{
+   impar++;
+}
 
-            somaNotas += notas[i][j];
-        }
-          
-    
-        fflush(stdin);
-        media[i] = somaNotas / j;
-        somaNotas = 0;
+ if (numeros[i] < 0){
+     numerosNegativos++;
     }
 
-    printf("\n Mostrando os Resultados \n");
+ if (numeros[i] < menor){
+    menor = numeros[i];
+ }else if(numeros[i] > maior){
+    maior = numeros[i];
+  }
 
-    for (i = 0; i < 5; i++)
-    {
-        printf("\n Nome do aluno: %s \n", nome[i]);
-        printf("Idade: %d \n", idade[i]);
-            
-        for (j = 0; j < 3; j++)
-        {
-            printf("Nota: %1.f \n", notas[i][j]);
-        } 
-
-
-        printf("Media: %2.f \n", media[i]);
-
-        if (media[i] >= 7)
-        {
-            printf("Aprovado! \n");
-        }
-        else if (media[i] >= 5 && media[i] < 7)
-        {
-            printf("Recuperação. \n");
-        }
-        else if (media[i] < 5)
-        {
-            printf("Reprovado. \n");
-        }
+  if (numeros[i] > 0){
+    somaGeral += numeros[i];
+ 
+}
+ if (par > 0) {
+        mediaPar = somaPar / (float) par;
     }
 
+}
+
+ mediaGeral = somaGeral / i;
+
+
+printf("Resultados: ");
+
+        printf("\n Quantidade de números impar %d", impar);
+        printf("\n Quantidade de números negativos %d", numerosNegativos);
+        printf("\n Maior número:  %d", maior);
+        printf("\n Menor número:  %d", menor);
+        printf("\n Média de números Pares:  %2.f", mediaPar);
+        printf("\n Média de números inseridos:  %2.f", mediaGeral);
+
+   
     return 0;
 }
