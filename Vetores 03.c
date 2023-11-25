a274d66a2d6b5ad373b2bf9063236f67ba4a6495
Wayundo <107221656+Wayundo@users.noreply.github.com>
2023-11-25 18:02:20 -0300

    Update Vetores 03.c

--- Vetores 03.c
+++ Vetores 03.c
@@ -3,18 +3,20 @@
 #include <locale.h>
 #include <string.h>
 
-#define MAX_CONTATOS 9999
-
-int main() {
+int main()
+{
     setlocale(LC_ALL, "portuguese");
 
     int opcao;
     int i;
-    int numero[MAX_CONTATOS];
-    char contato[MAX_CONTATOS][500];
-    int totalContatos =0;
+    int numero[9999];
+    char contato[9999][500];
+    int totalContatos = 0;
 
-    do {
+
+
+    do
+    {
         printf("\n Menu: \n");
         printf("1 - Adicionar Contato e Número \n");
         printf("2 - Mostrar Contatos \n");
@@ -22,39 +24,33 @@ int main() {
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
 
-        switch (opcao) {
-            case 1:
-                if (totalContatos < MAX_CONTATOS) {
-                    printf("Digite o nome do contato: ");
-                    getchar();  // Limpar o buffer de entrada
-                    fgets(contato[totalContatos], sizeof(contato[totalContatos]), stdin);
-                    contato[totalContatos][strcspn(contato[totalContatos], "\n")] = '\0';
-
-                    printf("Digite o número do contato: ");
-                    scanf("%d", &numero[totalContatos]);
-                    totalContatos++;
-                } else {
-                    printf("Limite de contatos atingido.\n");
+        switch (opcao)
+        {
+        case 1:
+        if (totalContatos < 9999){
+            printf("Digite o nome do contato: ");
+            scanf("%s", contato[totalContatos]);
+            printf("Digite o número do contato: ");
+            scanf("%d", &numero[totalContatos]);
+            totalContatos++;
+        }
+            break;
+        case 2:
+        if (totalContatos > 0) {
+            printf("\nContatos na agenda:\n");
+            for (i = 0; i < totalContatos; i++){
+            printf("Nome: %s, Número: %d\n", contato[i], numero[i]);
                 }
-                break;
-            case 2:
-                if (totalContatos > 0) {
-                    printf("\nContatos na agenda:\n");
-                    for (i = 0; i < totalContatos; i++) {
-                        printf("Nome: %s, Número: %d\n", contato[i], numero[i]);
-                    }
-                } else {
-                    printf("Nenhum contato cadastrado.\n");
-                }
-                break;
+            }
+            break;
             case 3:
                 printf("Finalizando.\n");
                 break;
             default:
                 printf("Opção inválida. Tente novamente.\n");
-        }
+            }
+      
+         } while (opcao != 3);
 
-    } while (opcao != 3);
-
-    return 0;
-}
+        return 0;
+    }
